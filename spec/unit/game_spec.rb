47a38e2bbe9b2d6059df3456058315d27d125bb1
Andres Agudelo <anagudelogu@gmail.com>
2022-08-01 15:10:41 -0500

    Add test outline for Game class

---
+++ spec/unit/game_spec.rb
@@ -0,0 +1,8 @@
+require_relative '../../lib/game'
+
+RSpec.describe Game do
+  it 'is an Item'
+  it 'Should have a multiplayer property'
+  it 'Should have a last_plated_at property'
+  it 'can be archived if is older than 10 years AND was last played 2 or more years ago'
+end
