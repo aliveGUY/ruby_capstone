2c91e95a512dce096f506451051c7c8bf31446e9
Andres Agudelo <anagudelogu@gmail.com>
2022-08-01 16:22:15 -0500

    Refactor type for published_date, can_be_archived method and item class

--- lib/item.rb
+++ lib/item.rb
@@ -1,37 +1,39 @@
+require 'date'
+
 class Item
   attr_reader :id, :archived, :genre, :author, :source, :label
   attr_accessor :publish_date
 
-  def initialize(publish_date)
+  def initialize(publish_date:)
     @id = Random.rand(1..100)
-    @publish_date = publish_date
+    @publish_date = Date.parse(publish_date)
     @archived = false
   end
 
   def add_genre(genre)
     @genre = genre
-    genre.items << self
+    genre.items.push(self) unless genre.items.include?(self)
   end
 
   def add_author(author)
     @author = author
-    author.items << self
+    author.items.push(self) unless author.items.include?(self)
   end
 
   def add_source(source)
     @source = source
-    source.items << self
+    source.items.push(self) unless source.items.include?(self)
   end
 
   def add_label(label)
     @label = label
-    label.items << self
+    label.items.push(self) unless label.items.include?(self)
   end
 
   private
 
   def can_be_archived?
-    return true if @publish_date > 10
+    return true if (Date.today - @publish_date) / 365 > 10
 
     false
   end
