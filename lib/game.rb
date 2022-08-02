b4a208e10c7267847ed4f32eb933a78cb65eac2b
Andres Agudelo <anagudelogu@gmail.com>
2022-08-02 16:13:52 -0500

    Implement preserving data of games

--- lib/game.rb
+++ lib/game.rb
@@ -4,8 +4,8 @@ require 'date'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
-  def initialize(publish_date:, multiplayer:, last_played_at:)
-    super(publish_date: publish_date)
+  def initialize(publish_date:, multiplayer:, last_played_at:, id: nil)
+    super(id: id, publish_date: publish_date)
     @multiplayer = multiplayer
     @last_played_at = Date.parse(last_played_at)
   end
