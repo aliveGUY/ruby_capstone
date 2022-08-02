c8f9965691b7a2af6fc8c3233d2e77dea75229f5
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-08-02 14:59:03 +0200

    Fix linter errors

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,14 +1,13 @@
-require_relative '../lib/book.rb'
+require_relative '../lib/book'
 
 RSpec.describe Book do
   context 'When testing book that can not be archived' do
-
-    book = Book.new('2020-02-02','Emilia', 'good')
+    book = Book.new('2020-02-02', 'Emilia', 'good')
     book_publisher = book.publisher
-    book_archive = book.move_to_archive
+    book_archived = book.archived
 
     it 'Book should not be move to archived' do
-      expect(book.archived).to eq false
+      expect(book_archived).to eq false
     end
 
     it 'Book publisher should be equal to "Emilia"' do
@@ -17,12 +16,12 @@ RSpec.describe Book do
   end
 
   context 'When testing book that can be archived' do
-    book = Book.new('2000-02-02','Emilia', 'bad')
+    book = Book.new('2000-02-02', 'Emilia', 'bad')
     book_archive = book.move_to_archive
     book_cover = book.cover_state
 
     it 'Book should not be move to archived' do
-      expect(book.archived).to eq true
+      expect(book_archive).to eq true
     end
 
     it 'Book publisher should be equal to "Emilia"' do
