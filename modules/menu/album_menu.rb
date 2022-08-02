8935d8c907dbc80e951a56047ade38fe53a9af32
Andres Agudelo <anagudelogu@gmail.com>
2022-08-02 16:12:53 -0500

    Add shared code to index file of the module

--- modules/menu/album_menu.rb
+++ modules/menu/album_menu.rb
@@ -2,11 +2,7 @@ module Menu
   def album_menu(app)
     puts 'Enter the publish date: [YYYY-MM-DD]'
     publish_date = gets.chomp
-    if publish_date.match(/\d{4}-(?:[01][0-2])-(?:[01][0-9]|2[0-9]|3[0-1])/).nil?
-      album_menu(app)
-    else
-      Date.parse(publish_date)
-    end
+    album_menu(app) if publish_date.match(/\d{4}-(?:[01][0-2])-(?:[01][0-9]|2[0-9]|3[0-1])/).nil?
     puts 'Is it on Spotify?: [Y/N]'
     on_spotify = gets.chomp
     on_spotify = on_spotify.upcase == 'Y'
