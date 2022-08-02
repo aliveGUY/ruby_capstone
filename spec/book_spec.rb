8a11b39cca51aadc118a4284beb192de82a62ecf
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-08-02 17:00:55 +0200

    Use let for variables in tests

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -2,23 +2,23 @@ require_relative '../lib/book'
 
 RSpec.describe Book do
   context 'When testing book that can not be archived' do
-    book = Book.new('2020-02-02', 'Emilia', 'good')
-    book_publisher = book.publisher
-    book_archived = book.archived
+    let(:book) { Book.new('2020-02-02', 'Emilia', 'good') }
+    let(:book_publisher) { book.publisher }
+    let(:book_archived) { book.archived }
 
     it 'Book should not be move to archived' do
       expect(book_archived).to eq false
     end
 
     it 'Book publisher should be equal to "Emilia"' do
-      expect(book_publisher).to eql('Emilia')
+      expect(book.publish_date).to eql(Date.parse('2020-02-02'))
     end
   end
 
   context 'When testing book that can be archived' do
-    book = Book.new('2000-02-02', 'Emilia', 'bad')
-    book_archive = book.move_to_archive
-    book_cover = book.cover_state
+    let(:book) { Book.new('2000-02-02', 'Emilia', 'bad') }
+    let(:book_archive) { book.move_to_archive }
+    let(:book_cover) { book.cover_state }
 
     it 'Book should not be move to archived' do
       expect(book_archive).to eq true
