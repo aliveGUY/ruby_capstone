c1c79a254b2073617ce9eefac2b205dc154d1f77
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-08-02 17:17:11 +0200

    Fix tests for book

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe Book do
   context 'When testing book that can not be archived' do
     let(:book) { Book.new('2020-02-02', 'Emilia', 'good') }
     let(:book_publisher) { book.publisher }
-    let(:book_archived) { book.archived }
 
     it 'Book should not be move to archived' do
-      expect(book_archived).to eq false
+      book.move_to_archive
+      expect(book.archived).to eq false
     end
 
     it 'Book publisher should be equal to "Emilia"' do
@@ -17,11 +17,11 @@ RSpec.describe Book do
 
   context 'When testing book that can be archived' do
     let(:book) { Book.new('2000-02-02', 'Emilia', 'bad') }
-    let(:book_archive) { book.move_to_archive }
     let(:book_cover) { book.cover_state }
 
     it 'Book should not be move to archived' do
-      expect(book_archive).to eq true
+      book.move_to_archive
+      expect(book.archived).to eq true
     end
 
     it 'Book publisher should be equal to "Emilia"' do
